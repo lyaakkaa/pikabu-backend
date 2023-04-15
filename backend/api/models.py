e4c826f1392e14e0d01c18a438423c6b84cba895
Leila Bekturgan <leila.bekturgan@gmail.com>
2023-04-15 23:09:35 +0600

    new commit

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,16 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self) -> str:
+        return self.name
+
 class User(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
 
-class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    comment_likes = models.IntegerField(default=0)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self) -> str:
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -21,8 +31,27 @@ class Post(models.Model):
     post_likes = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    comment_likes = models.IntegerField(default=0)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+
 
 
 
---
+++ backend/api/serializers.py
@@ -0,0 +1,47 @@
+
+from rest_framework import serializers
+from .models import *
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+
+class CategorySerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
+ class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255, required=True)
+    email = serializers.EmailField(required=True)
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
