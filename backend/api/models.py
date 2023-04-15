95cac16f112bc06e2f08aef611c70be235783f99
Leila Bekturgan <leila.bekturgan@gmail.com>
2023-04-15 22:49:34 +0600

    add models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    comment_likes = models.IntegerField(default=0)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created_date = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(default='')
+    post_likes = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+
+
+
